b9e45e8b9bda553e043eb7954ec4ae82eb64e985
jacob coffman <jacob.a.coffman@boeing.com>
2012-02-06 23:44:42 -0600

    Updated for arduino 1.0.

---
+++ comm.ino
@@ -0,0 +1,124 @@
+/*************************************************************
+Arduino Alarm by jake@coffshire.com
+*************************************************************/
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// Arduino network information
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x02 }; 
+EthernetClient client;
+char smtpServer[] = "smtp.charter.net";
+char google[] = "www.google.com";
+
+// Call to initialize the communication with the email server
+void setupComm()
+{
+  Serial.println("Trying to connect");
+  playNote('a', 250);
+  if (!Ethernet.begin(mac))
+  {
+    playNote('g', 250);
+    Serial.println("Failed to DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    while(true);
+  }
+  playNote('b', 250);
+  delay(1000);
+  
+  // print your local IP address:
+  Serial.print("My IP address: ");
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    // print the value of each byte of the IP address:
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print("."); 
+  }
+  Serial.println();
+  
+  if(email("Arduino is up!"))
+  {
+    playNote('c', 250);
+  }
+  else
+  {
+    playNote('g', 250);
+    delay(2000);
+  }
+}
+    
+// Call to send an email. 
+bool email(char* text)
+{
+  bool success = false;
+  Serial.println("Sending email...");
+
+  if(client.connect(google, 80))
+  {
+    Serial.println("Got google");
+    client.stop();
+  }
+  else
+  {
+    Serial.println("Not google");
+  }
+
+  if (client.connect(smtpServer, 25)) 
+  {
+    Serial.println("connected");
+    delay(100);
+    client.println("HELO arduino"); 
+    for(int i=0; i<999; ++i)
+    {
+      if(client.read() > 0)
+        break;
+    } 
+    Serial.println("responded");
+    // Put your "from" email address here
+    client.println("MAIL FROM: arduino@coffshire.com"); 
+    for(int i=0; i<999; ++i)
+    {
+      if(client.read() > 0)
+        break;
+    } 
+    Serial.println("mail from'd");
+    // Put where you are sending it here
+    client.println("RCPT TO: jake@coffshire.com");
+    for(int i=0; i<999; ++i)
+    {
+      if(client.read() > 0)
+        break;
+    } 
+    Serial.println("rcpt");
+    client.println("DATA"); 
+    for(int i=0; i<999; ++i)
+    {
+      if(client.read() > 0)
+        break;
+    } 
+
+    client.println("From: arduino@coffshire.com");
+    client.println("To: jake.coffman@gmail.com");  
+    client.println("Subject: From your arduino"); 
+
+    client.println(text); 
+    client.println("."); 
+
+    client.println("QUIT"); 
+    for(int i=0; i<999; ++i)
+    {
+      if(client.read() > 0)
+        break;
+    } 
+    Serial.println("datad");
+    success = true;
+    client.println();
+
+  } 
+  else 
+  {
+    Serial.println("Failed");
+    client.println("QUIT"); //attempt to cleanup the connection
+  }
+  client.stop();
+  return success;
+}
---
+++ sounds.ino
@@ -0,0 +1,44 @@
+/*************************************************************
+Arduino Alarm by jake@coffshire.com
+*************************************************************/
+
+// Sound related stuff
+int length = 2; // the number of notes
+
+int beats[] = { 1, 4 };  // hold the second note
+int tempo = 200;         // relatively slow
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void playNote(char note, int duration) {
+  // play the tone corresponding to the note name
+  for (int i = 0; i < 8; i++) {
+    if (names[i] == note) {
+      playTone(tones[i], duration);
+    }
+  }
+}
+
+void play(const char notes[]) {
+    for (int i = 0; i < length; i++) 
+    {
+      if (notes[i] == ' ') 
+      {
+        delay(beats[i] * tempo); // rest
+      } 
+      else 
+      {
+        playNote(notes[i], beats[i] * tempo);
+      }
+    
+      // pause between notes
+      delay(tempo / 2); 
+    }  
+}
